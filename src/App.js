642edce1e37fb1257b0fd4d044043308dee1335d
Damon Edmons <dsedmons@gmail.com>
2022-10-13 00:16:22 -0400

    feat:added text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test project I am making all by myself. #feelsAmazing
         </p>
         <a
           className="App-link"
